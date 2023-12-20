5b9afd0769b9030ecbb401c597ea702947a11b43
Haseeb0334 <144684254+Haseeb0334@users.noreply.github.com>
2023-12-20 11:58:48 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/Header';
+import { HeroSection } from './Components/HeroSection';
+import Footer from './Components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/HeroSection.js
@@ -0,0 +1,8 @@
+export const HeroSection = () => 
+{
+    return(
+        <>
+            <h1>Hero Section</h1>
+        </>
+    )
+}
